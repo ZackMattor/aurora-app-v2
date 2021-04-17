8ec725a9cf5fa8c6c73aa50588913b5cfec45d41
Zack Mattor <zmattor@gmail.com>
2021-04-17 00:05:53 -0400

    vuexify and componentify some things under the lights page :D

---
+++ src/js/modules/lights/views/components/light_row.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="select-none border-4 border-solid border-gray-400 rounded-md flex flex-1 items-center p-4">
+    <div class="flex flex-col w-full">
+      <div class="flex flex-1 items-center">
+        <div class="flex flex-col rounded-md w-20 h-20 bg-gray-600 justify-center items-center mr-4 text-5xl"><i :class="geometryIcon"></i></div>
+
+        <div class="flex-1 pl-1 mr-15">
+          <div class="font-medium text-xl">{{ light.id }}</div>
+          <div class="text-gray-600 text-sm">{{ animationsByGeometry(light.geometry_name).length }} Animations</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+import axios from 'axios';
+
+export default {
+  props: ['light'],
+
+  computed: {
+    geometryIcon() {
+      let geo = this.geometryByKey(this.light.geometry_name);
+      return geo ? geo.icon : 'fas fa-question';
+    },
+
+    ...mapState('animations', {
+      animations: state => state.all
+    }),
+
+    ...mapState('geometries', {
+      geometries: state => state.all
+    }),
+
+    ...mapGetters('geometries', {
+      geometryByKey: 'getByKey'
+    }),
+
+    ...mapGetters('animations', {
+      animationsByGeometry: 'getByGeometry'
+    })
+  }
+}
+</script>
--- src/js/modules/lights/views/index.vue
+++ src/js/modules/lights/views/index.vue
@@ -1,53 +1,29 @@
 <template>
   <ul class="flex flex-col p-4">
     <li v-for="light in lights" class="border-gray-400 flex flex-row mb-2">
-      <div class="select-none border-4 border-solid border-gray-400 rounded-md flex flex-1 items-center p-4">
-        <div class="flex flex-col w-full">
-          <div class="flex flex-1 items-center">
-            <div class="flex flex-col rounded-md w-20 h-20 bg-gray-600 justify-center items-center mr-4 text-5xl"><i :class="geometryByKey(light.geometry_name).icon"></i></div>
-
-            <div class="flex-1 pl-1 mr-15">
-              <div class="font-medium text-xl">{{ light.id }}</div>
-              <div class="text-gray-600 text-sm">{{ animationsByGeometry(light.geometry_name).length }} Animations</div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <light-row :light="light"></light-row>
     </li>
   </ul>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex';
-import axios from 'axios';
+
+import LightRow from './components/light_row.vue';
 
 export default {
   async mounted() {
-    console.log('Fetching lights...')
-    this.lights = (await axios.get('http://10.0.0.20:8080/api/lights')).data;
-    console.log(this.lights);
-  },
-
-  data() {
-    return { lights: [] };
+    this.$store.dispatch('lights/fetch');
   },
 
   computed: {
-    ...mapState('animations', {
-      animations: state => state.all
-    }),
-
-    ...mapState('geometries', {
-      geometries: state => state.all
-    }),
-
-    ...mapGetters('geometries', {
-      geometryByKey: 'getByKey'
-    }),
-
-    ...mapGetters('animations', {
-      animationsByGeometry: 'getByGeometry'
+    ...mapGetters('lights', {
+      lights: 'all'
     })
+  },
+
+  components: {
+    "light-row": LightRow
   }
 }
 </script>
